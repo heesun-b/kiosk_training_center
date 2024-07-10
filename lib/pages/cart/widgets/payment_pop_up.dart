100ec84b301889281bfa2e679ca1e5ed3ff86f66
heesun <116797781+heesun-b@users.noreply.github.com>
2024-07-10 17:36:22 +0900

    작업중

--- lib/pages/cart/widgets/payment_pop_up.dart
+++ lib/pages/cart/widgets/payment_pop_up.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:kiosk_training_center/constants/colours.dart';
 import 'package:kiosk_training_center/pages/cart/widgets/payment_fifth_step.dart';
 import 'package:kiosk_training_center/pages/cart/widgets/payment_first_step.dart';
@@ -18,10 +19,24 @@ class PaymentPopUp extends StatefulWidget {
 }
 
 class _PaymentPopUpState extends State<PaymentPopUp> {
+  AudioPlayer player = AudioPlayer();
+  var isPlayedAudio = false;
 
   @override
   void initState() {
     super.initState();
+    playAudio();
+  }
+
+  void playAudio()  {
+
+    setState(() {
+      isPlayedAudio = true;
+    });
+
+      player.setAsset('assets/audios/sixth.mp3');
+      player.setVolume(1.0);
+      player.play();
   }
 
   @override
